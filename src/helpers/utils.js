cc8545e65926d83f58c49190c8a2d0c3f3478348
pratapshivendra817 <71538947+pratapshivendra817@users.noreply.github.com>
2021-02-17 17:43:40 +0530

    react project

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,12 +2,12 @@ export function getFormBody(params) {
     let formBody = [];
   
     for (let property in params) {
-      let encodedKey = encodeURIComponent(property); //'user name=>'user%20name'
-      let encodedValue = encodeURIComponent(params[property]); // shashank 22 => shashank%2022
+      let encodedKey = encodeURIComponent(property); 
+      let encodedValue = encodeURIComponent(params[property]); 
       formBody.push(encodedKey + '=' + encodedValue);
     }
     console.log('formurl', formBody.join('&'));
-    return formBody.join('&'); //'username=shashank&password=1222'
+    return formBody.join('&'); 
   }
   export function getAuthTokenFromLocalStorage() {
     return localStorage.getItem('token');
